4f50326be847883cf578157dbb30896dba05e816
olly <olly@ollycope.com>
2014-02-02 15:09:32 +0000

    Promote tests into a package

--- yoyo/tests/test_migrations.py
+++ yoyo/tests/test_migrations.py
@@ -1,44 +1,8 @@
-import re
-from tempfile import mkdtemp
-from shutil import rmtree
-from functools import wraps
-import os
-
 from yoyo.connections import connect
 from yoyo import read_migrations
 from yoyo import DatabaseError
 
-dburi = "sqlite:///:memory:"
-
-
-def with_migrations(*migrations):
-    """
-    Decorator taking a list of migrations. Creates a temporary directory writes
-    each migration to a file (named '0.py', '1.py', '2.py' etc), calls the
-    decorated function with the directory name as the first argument, and
-    cleans up the temporary directory on exit.
-    """
-
-    def unindent(s):
-        initial_indent = re.search(r'^([ \t]*)\S', s, re.M).group(1)
-        return re.sub(r'(^|[\r\n]){0}'.format(re.escape(initial_indent)),
-                      r'\1', s)
-
-    def decorator(func):
-        tmpdir = mkdtemp()
-        for ix, code in enumerate(migrations):
-            with open(os.path.join(tmpdir, '{0}.py'.format(ix)), 'w') as f:
-                f.write(unindent(code).strip())
-
-        @wraps(func)
-        def decorated(*args, **kwargs):
-            try:
-                func(tmpdir, *args, **kwargs)
-            finally:
-                rmtree(tmpdir)
-
-        return decorated
-    return decorator
+from yoyo.tests import with_migrations, dburi
 
 
 @with_migrations(
