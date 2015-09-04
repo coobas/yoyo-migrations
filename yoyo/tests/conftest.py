dc2ff16a59b4ee2baf95af8088bb09d5ad58dd88
olly <olly@ollycope.com>
2015-09-04 12:02:52 +0000

    Ensure backend_fixture returns a fresh backend instance everytime
    The backend object needs to be initialized fresh everytime in order to
    recreate the _yoyo_migrations table, which is dropped in the fixture teardown.

--- yoyo/tests/conftest.py
+++ yoyo/tests/conftest.py
@@ -1,19 +1,20 @@
 import pytest
 
-from yoyo.tests import get_test_backends
+from yoyo.connections import get_backend
+from yoyo.tests import get_test_dburis
 
 
-@pytest.yield_fixture(params=get_test_backends())
+@pytest.yield_fixture(params=get_test_dburis())
 def backend_fixture(request):
     """
     Return all backends configured in ``test_databases.ini``
     """
-    backend = request.param
+    backend = get_backend(request.param)
     try:
         yield backend
     finally:
         backend.rollback()
-        for table in (request.param.list_tables()):
+        for table in (backend.list_tables()):
             if table.startswith('_yoyo'):
                 with backend.transaction():
                     backend.execute("DROP TABLE {}".format(table))
