3d301c3d70f43d320a570b85054f010cd3c73865
Olly Cope <olly@ollycope.com>
2018-01-29 14:37:50 +0000

    Fix error trying to save database urls in the config that contain percents

--- yoyo/scripts/main.py
+++ yoyo/scripts/main.py
@@ -262,7 +262,8 @@ def main(argv=None):
     if args.sources:
         config.set('DEFAULT', 'sources', ' '.join(args.sources))
     if args.database:
-        config.set('DEFAULT', 'database', args.database)
+        # ConfigParser requires that any percent signs in the db uri be escaped.
+        config.set('DEFAULT', 'database', args.database.replace('%', '%%'))
     config.set('DEFAULT', 'migration_table', args.migration_table)
     config.set('DEFAULT', 'batch_mode', 'on' if args.batch_mode else 'off')
     config.set('DEFAULT', 'verbosity', str(args.verbosity))
