3e74360721c58e5ec81b5a5adb515447dde9bedf
Olly Cope <olly@ollycope.com>
2018-06-12 10:39:02 +0000

    Move setuptools metadata into setup.cfg and add classifiers

--- setup.py
+++ setup.py
@@ -12,14 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-import os
 import re
-from setuptools import setup, find_packages
+from setuptools import setup
 
 VERSIONFILE = "yoyo/__init__.py"
-install_requires = ['text-unidecode', 'iniherit']
 
 
 def get_version():
@@ -28,39 +24,4 @@ def get_version():
                            f.read().decode('UTF-8'), re.M).group(1)
 
 
-def read(*path):
-    """
-    Return content from ``path`` as a string
-    """
-    with open(os.path.join(os.path.dirname(__file__), *path), 'rb') as f:
-        return f.read().decode('UTF-8')
-
-
-setup(
-    name='yoyo-migrations',
-    version=get_version(),
-    description='Database schema migration tool using SQL and DB-API',
-    long_description=read('README.rst'),
-    url='https://bitbucket.org/ollyc/yoyo',
-    author='Oliver Cope',
-    author_email='oliver@redgecko.org',
-    license='Apache',
-    keywords=['migrations', 'migrate', 'database', 'db', 'alembic', 'south',
-              'mysql', 'postgresql', 'sqlite'],
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    extras_require={
-        'mysql': ['PyMySQL'],
-        'postgres': ['psycopg2'],
-        'pyodbc': ['pyodbc']
-    },
-    tests_require=['mock'],
-    entry_points={
-        'console_scripts': [
-            'yoyo=yoyo.scripts.main:main',
-            'yoyo-migrate=yoyo.scripts.main:main',
-        ],
-    }
-)
+setup(name='yoyo-migrations', version=get_version())
